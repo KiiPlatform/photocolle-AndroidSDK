3a3a2f0ff1d2fbe698679008a288c0154c89dca7
circleci <circleci@kii.com>
2016-08-25 20:37:14 +0900

    Updated script.

--- circleci_scripts/javadoc_upload.sh
+++ circleci_scripts/javadoc_upload.sh
@@ -1,13 +1,12 @@
 #! bin/bash
 
-git config --global user.name "circleci"
-git config --global user.email "circleci@kii.com"
 git clone git@github.com:KiiPlatform/photocolle-AndroidSDK.git RepoForDoc
 
 cd RepoForDoc
 git checkout gh-pages
 rm -fr docs/
 cp -fr ../photocolle-sdk/sdk/build/docs/javadoc docs
+git add docs
 git commit -a -m "update api doc"
 git push origin gh-pages
 
