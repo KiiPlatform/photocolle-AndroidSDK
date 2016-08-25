27e4a7c7d7e708205ffcc0288b9dd8e58bbff511
satoshi <satoshi.kumano@kii.com>
2016-08-25 18:02:45 +0900

    Fixed script.

--- circleci_scripts/sdk_upload.sh
+++ circleci_scripts/sdk_upload.sh
@@ -1,6 +1,6 @@
 # get the aar file name to upload
 version=$(cat gradle.properties | sed -e 's/photocolleSDKVersion=//g')
-sdk_body_file="../target/PhotoColleSDK-Android-$version.zip"
+sdk_body_file="target/PhotoColleSDK-Android-$version.zip"
 
 if [ -f "$sdk_body_file" ]; then
 
@@ -9,9 +9,9 @@ if [ -f "$sdk_body_file" ]; then
     cd internal-tools/sdk-uploader
 
     sdk_meta_file=../../sdk-metadata.json
-    sdk_body_file=../../$sdk_body_file
+    sdk_body_file="../../$sdk_body_file"
 
-    if [ $SDK_DEST == 'release' ]; then
+    if [ "$SDK_DEST" == 'release' ]; then
         # release to kii cloud server
         echo "release to kii cloud"
         python sdk_uploader.py --meta=$sdk_meta_file --file=$sdk_body_file --content-type=application/zip --kii-bucket=sdks
@@ -21,7 +21,7 @@ if [ -f "$sdk_body_file" ]; then
         cd ../../
         ./gradlew bintrayUpload -Pbintray_token=$BINTRAY_TOKEN -Pbintray_user=$BINTRAY_USER
 
-    elif [ $SDK_DEST == 'test' ]; then
+    elif [ "$SDK_DEST" == 'test' ]; then
         python sdk_uploader.py --meta=$sdk_meta_file --file=$sdk_body_file --content-type=application/zip
         echo "test"
         cd ../../
@@ -29,7 +29,6 @@ if [ -f "$sdk_body_file" ]; then
         echo "SDK_DEST should either release or test"
         exit 1
     fi
-    echo "Succeeded to release sdk"
 else
     echo "no release aar file($sdk_body_file) found to upload"
     exit 1
