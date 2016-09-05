515c522227fae749cc197bdafdd1f99e706867dc
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-09-05 16:22:46 +0900

    Retring HTTP is disabled.

--- photocolle-sdk/sdk/src/main/java/com/kii/sdk/photocolle/HttpClientFactory.java
+++ photocolle-sdk/sdk/src/main/java/com/kii/sdk/photocolle/HttpClientFactory.java
@@ -5,10 +5,24 @@ import com.squareup.okhttp.apache.OkApacheClient;
 
 import org.apache.http.client.HttpClient;
 
+import java.util.concurrent.TimeUnit;
+
 //For internal use
 class HttpClientFactory {
 
-    HttpClient createClient() {
-        return new OkApacheClient(new OkHttpClient());
+    private static HttpClient CLIENT = null;
+
+    synchronized HttpClient createClient() {
+        if (CLIENT != null) {
+            return CLIENT;
+        }
+        OkHttpClient client = new OkHttpClient();
+        client.setRetryOnConnectionFailure(true);
+        CLIENT = new OkApacheClient(client);
+        client.setConnectTimeout(30, TimeUnit.SECONDS);
+        client.setReadTimeout(30, TimeUnit.SECONDS);
+        client.setWriteTimeout(30, TimeUnit.SECONDS);
+        return CLIENT;
+
     }
 }
